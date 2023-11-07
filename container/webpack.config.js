f25e37244e51815dda79971ac5d31bb4aa1ae9c3
nuzzesick <mati280341@gmail.com>
2023-11-07 02:33:01 +0100

    chore: modified html

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,13 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  module: "development",
+  mode: "development",
   devServer: {
     port: 8080
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './'
+      template: "./public/index.html"
     })
   ]
 }
